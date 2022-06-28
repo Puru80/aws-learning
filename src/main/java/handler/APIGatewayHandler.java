f95e8a6035136ee3bed8fbc7857ddc1abdb45bb2
Puru80 <puru.agar99@gmail.com>
2022-06-28 18:32:51 +0530

    Template file added

--- src/main/java/handler/APIGatewayHandler.java
+++ src/main/java/handler/APIGatewayHandler.java
@@ -2,16 +2,25 @@ package handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+public class APIGatewayHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-public class APIGatewayHandler implements RequestStreamHandler {
-
-    @Override
+    /*@Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         System.out.println("Hi ! ! ! ! I am invoked by APIGateway");
+    }*/
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        APIGatewayProxyResponseEvent event = new APIGatewayProxyResponseEvent();
+
+        event.setStatusCode(200);
+        event.setBody(apiGatewayProxyRequestEvent.getBody());
+        event.setHeaders(null);
+        event.setIsBase64Encoded(true);
+
+        return event;
     }
 }
